1a3d1742df4acd85a7b4899d25b3e83bb4498b3a
Raleigh Hooper <raleighhooper@Raleighs-MacBook-Pro.local>
2023-04-09 18:38:42 -0700

    Second commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,31 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 const Weather = () => {
-  const [data, setData] = useState({})
+  const [apiData, setApiData] = useState()
+  const [search, setSearch] = useState("")
 
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=burbank&units=imperial&appid=dede7a4a009a8e66bd2a80d8502e5f08'
+  // const url = 'https://api.openweathermap.org/data/2.5/weather?q=burbank&units=imperial&appid=dede7a4a009a8e66bd2a80d8502e5f08'
   
   useEffect(() => {
-   const interval = setInterval(() => {
-       axios.get(url)
-       .then(res => setData(res.data))
-       .catch(err => console.log(err));
-     }, 5000);
-    
-
-    return () => clearInterval(interval);
-  },[]);
+      const fetchData = async () => {
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=imperial&appid=dede7a4a009a8e66bd2a80d8502e5f08`)
+        const data = await res.json()
+        console.log(data);
+        if(res.ok){
+          setApiData(data)
+        }
+      }
+      fetchData()
+  }, [search])
 
   return (
       <div className='box'>
-        <h1>Burbank <span>Weather</span></h1>
-        <div className='element'>
-            <img src={"http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"} alt="weather" />
-          <p><span>Temp: {data.main ? <span>{data.main.temp.toFixed()}°F</span> : null}</span></p>
-          <p><span>Humidity: {data.main ? <span>{data.main.humidity}%</span> : null}</span></p>
-          <p><span>Wind: {data.wind ? <span>{data.wind.speed.toFixed()} mph</span> : null}</span></p>
+        <h1>Weather <span>Check</span></h1>
+        <div className="search">
+          <input 
+            type="text" 
+            name="search" 
+            placeholder='search' 
+            onChange={(e)=>setSearch(e.target.value)} 
+            value={search} 
+          />
         </div>
+        {apiData? <div className='bottom'>
+            <img src={"http://openweathermap.org/img/w/" + apiData.weather[0].icon + ".png"} alt="weather" />
+          <p><span>Temp: </span> {apiData.main.temp.toFixed()}°F</p>
+          <p><span>Humidity: </span> {apiData.main.humidity.toFixed()}%</p>
+          <p><span>Wind: </span> {apiData.wind.speed.toFixed()} mph</p>
+          <p>{apiData.name}</p>
+        </div>
+        : <h1>No data found.</h1>}
       </div>
     )
 
